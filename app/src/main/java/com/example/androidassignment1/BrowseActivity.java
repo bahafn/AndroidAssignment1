7dbeb824a55257f644dc7da57d8569ae0b1b8441
bahafn <103534448+bahafn@users.noreply.github.com>
2025-04-13 22:50:38 +0300

    Added basic item browsing.

--- app/src/main/java/com/example/androidassignment1/BrowseActivity.java
+++ app/src/main/java/com/example/androidassignment1/BrowseActivity.java
@@ -1,8 +1,12 @@
 package com.example.androidassignment1;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +14,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.androidassignment1.DataAccess.Item;
+import com.example.androidassignment1.DataAccess.ItemDAFactory;
+import com.example.androidassignment1.DataAccess.iItemDA;
+
+import java.util.List;
+
 public class BrowseActivity extends AppCompatActivity {
+    private ListView lvItems;
+    private SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +34,23 @@ public class BrowseActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        setupViews();
+        showItems();
+    }
+
+    private void setupViews() {
+        lvItems = findViewById(R.id.lvItems);
+    }
+
+    private void showItems() {
+        iItemDA itemDA = ItemDAFactory.getInstance(sharedPreferences);
+        List<Item> items = itemDA.getAllItems();
+
+        ArrayAdapter<Item> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(adapter);
     }
 
     public void searchOnClick(View view) {
